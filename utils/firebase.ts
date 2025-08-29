9d77e513c2a051972a822e3e5f31e6966068f477
johnboyalone <73130409+johnboyalone@users.noreply.github.com>
2025-08-29 20:00:15 +0700

    Add files via upload

---
+++ utils/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// =================================================================================
+// TODO: PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
+// คุณสามารถหาค่าเหล่านี้ได้จากหน้า Project Settings ใน Firebase Console ของคุณ
+// =================================================================================
+const firebaseConfig = {
+  apiKey: "AIzaSyCNhlBoxWAvuFxol5YZM5zbcS6MAxiusx0",
+  authDomain: "tailek.firebaseapp.com",
+  projectId: "tailek",
+  storageBucket: "tailek.firebasestorage.app",
+  messagingSenderId: "532397147592",
+  appId: "1:532397147592:web:708de36fcf69af95caacfd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the Firestore database service
+export const db = getFirestore(app);
